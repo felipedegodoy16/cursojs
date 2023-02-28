ab96bbba56025e6e21fa4a3925c8979ca2963f48
Felipe Godoy <felipedegodoy.dev@gmail.com>
2023-02-27 23:23:17 -0300

    aula13

---
+++ ambiente.js
@@ -0,0 +1,22 @@
+var c = 1
+do {
+    console.log('Tudo bem!')
+    c ++
+} while (c < 6) 
+
+/*
+var c = 1
+while (c < 11) {
+    console.log('Tudo bem!')
+    c ++
+}
+*/
+
+/*
+console.log('Tudo bem!')
+console.log('Tudo bem!')
+console.log('Tudo bem!')
+console.log('Tudo bem!')
+console.log('Tudo bem!')
+console.log('Tudo bem!')
+*/
